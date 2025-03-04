e1fbc0d8681e8482b03859ba6b5349aae4354de4
EmmaCartuyvels1 <emma_cartuyvels@hotmail.com>
2025-03-04 11:39:54 +0100

    Create functions to add observation cyclus and correct species names

--- source/pipelines/exploratory_analysis/R/read_data.R
+++ source/pipelines/exploratory_analysis/R/read_data.R
@@ -1,3 +1,33 @@
 path_to_interim <- function(path_to_data, file) {
   file.path(path_to_data, "interim", file)
 }
+
+add_cyclus <- function(data){
+  output <- data |>
+    mutate(cyclus = case_when(
+      year >= 2007 & year <= 2009 ~ 1,
+      year >= 2010 & year <= 2012 ~ 2,
+      year >= 2013 & year <= 2015 ~ 3,
+      year >= 2016 & year <= 2018 ~ 4,
+      year >= 2019 & year <= 2021 ~ 5,
+      year >= 2022 & year <= 2024 ~ 6
+  ))
+
+  return(output)
+}
+
+rename_species <- function(data){
+  output <- data |>
+    mutate(species = case_when(
+      species == "Parus montanus" ~ "Poecile montanus",
+      species == "Dendrocopus major" ~ "Dendrocopos major",
+      species == "Saxicola torquatus" ~ "Saxicola rubicola",
+      TRUE ~ species
+    ))
+
+  return(output)
+}
+
+filter_1 <- function(data){
+
+}
