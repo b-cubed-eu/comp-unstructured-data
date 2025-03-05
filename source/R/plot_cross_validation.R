51a63dae36526c1a80299bf332a84a5632d459f5
Ward Langeraert <ward.langeraert@inbo.be>
2025-03-05 14:12:53 +0100

    coding style

--- source/R/plot_cross_validation.R
+++ source/R/plot_cross_validation.R
@@ -1,7 +1,9 @@
 plot_cross_validation <- function(
     cv_df,
     prevalence_df,
-    measure = ".data[[measure]]",
+    x = "abv",
+    y = "birdcube",
+    measure = "rmse",
     quant = 0.9,
     max.overlaps = 20) {
   require("dplyr")
@@ -10,14 +12,14 @@ plot_cross_validation <- function(
   require("ggrepel")
 
   cv_df %>%
-    distinct(species, rarity, !!sym(measure)) %>%
-    left_join(prevalence_df, by = join_by(species, rarity)) %>%
+    distinct(.data$species, .data$rarity, !!sym(measure)) %>%
+    left_join(prevalence_df, by = join_by("species", "rarity")) %>%
     mutate(measure_quant = stats::quantile(.data[[measure]], probs = quant)) %>%
-    ggplot(aes(x = abv, y = birdcube)) +
-    geom_point(aes(shape = rarity, colour = .data[[measure]]), size = 2) +
+    ggplot(aes(x = .data[[x]], y = .data[[y]])) +
+    geom_point(aes(shape = .data$rarity, colour = .data[[measure]]), size = 2) +
     ggrepel::geom_text_repel(
-      aes(label = ifelse(.data[[measure]] > measure_quant,
-                         paste0(species, "\n(value: ",
+      aes(label = ifelse(.data[[measure]] > .data$measure_quant,
+                         paste0(.data$species, "\n(value: ",
                                 round(.data[[measure]], 3), ")"),
                          NA)
           ),
