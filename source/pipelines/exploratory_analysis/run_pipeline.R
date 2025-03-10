72422345d4fd3cef66a3d73bffc14713ac50a31c
EmmaCartuyvels1 <emma_cartuyvels@hotmail.com>
2025-03-10 17:23:31 +0100

    Add code for debugging

--- source/pipelines/exploratory_analysis/run_pipeline.R
+++ source/pipelines/exploratory_analysis/run_pipeline.R
@@ -1,9 +1,9 @@
 # run the pipeline
-
 library(targets)
 Sys.setenv(TAR_PROJECT = "exploratory_analysis")
 
 tar_make()
+
 
 
 # inspect pipeline
@@ -13,5 +13,23 @@ tar_visnetwork()
 meta <- tar_meta()
 
 
+
 # debug pipeline
+# R console
+library(targets)
+
+# Restart your R session.
+rstudioapi::restartSession()
+
+# Loads globals like tar_option_set() packages, simulate_data(), and analyze_data():
+tar_load_globals()
+
+# Load the data that the target depends on.
+tar_load(dataset1)
+
+# Run the command of the errored target.
+analyze_data(dataset1)
+#> Error in na.fail.default(list(measurement = c(1L, 2L, 3L, 4L, 1L, 2L,  :
+#>   missing values in object
+
 abv <- tar_read(abv)
