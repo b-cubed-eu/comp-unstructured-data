5a3f078c4ab55dce2f8bc348a850d06886c59c36
EmmaCartuyvels1 <emma_cartuyvels@hotmail.com>
2025-04-11 14:25:42 +0200

    Simplify pipeline with branching

--- source/pipelines/exploratory_analysis/_targets.R
+++ source/pipelines/exploratory_analysis/_targets.R
@@ -5,7 +5,7 @@ library(targets)
 # Set target options:
 tar_option_set(
   packages = c(
-               "dplyr"
+               "tidyverse"
                ),
   format = "qs" # Optionally set the default storage format. qs is fast.
 )
@@ -29,96 +29,65 @@ tar_source(file.path(targets_project_dir, "exploratory_analysis", "R"))
 # List of targets:
 list(
   tar_target(
-    abv_datasets,
-    c("abv_data_cube.csv", "abv_data_cube_10km.csv")
-  ),
-  tarchetypes::tar_file(
-    abv_data_file,
-    path_to_interim(path_to_data = path_to_data, file = abv_datasets),
-    pattern = map(abv_datasets)
-  ),
-  tar_target(
-    abv_data_int1,
-    read.csv(abv_data_file),
-    pattern = map(abv_data_file)
-  ),
-  tar_target(
-    abv_data_int2,
-    add_cyclus(abv_data_int1),
-    pattern = map(abv_data_int1)
-  ),
-  tar_target(
-    abv_data,
-    add_category(abv_data_int2),
-    pattern = map(abv_data_int2)
-  ),
-  tar_target(
-    birdcube_datasets,
-    c("birdcubeflanders.csv", "birdcubeflanders_10km.csv")
-  ),
-  tarchetypes::tar_file(
-    birdcube_data_file,
-    path_to_interim(path_to_data = path_to_data, file = birdcube_datasets),
-    pattern = map(birdcube_datasets)
-  ),
-  tar_target(
-    birdcube_data_int,
-    read.csv(birdcube_data_file),
-    pattern = map(birdcube_data_file)
-  ),
-  tar_target(
-    birdcube_data,
-    add_cyclus(birdcube_data_int),
-    pattern = map(birdcube_data_int)
-  ),
-  tar_target(
     time_period,
     c("year", "cyclus")
   ),
   tar_target(
-    filter1_abv,
-    filter_1(abv_data),
-    pattern = map(abv_data)
+    spat_res,
+    c("1km", "10km")
   ),
   tar_target(
-    filter1_cube,
-    filter_1(birdcube_data),
-    pattern = map(birdcube_data)
+    dataset,
+    c("abv_data", "birdflanders")
+  ),
+  tarchetypes::tar_file(
+    data_file,
+    path_to_interim(path_to_data = path_to_data,
+                    dataset = dataset,
+                    spat_res = spat_res),
+    pattern = cross(dataset, spat_res)
   ),
   tar_target(
-    filter2_abv,
-    filter_2(abv_data, time_period),
-    pattern = cross(abv_data, time_period)
+    data_int1,
+    read_andid(data_file, dataset, spat_res),
+    pattern = map(data_file, cross(dataset, spat_res))
   ),
   tar_target(
-    filter2_cube,
-    filter_2(birdcube_data, time_period),
-    pattern = cross(birdcube_data, time_period)
+    data_int2,
+    add_cyclus(data_int1),
+    pattern = map(data_int1)
   ),
   tar_target(
-    filter3_abv,
-    filter_3(abv_data, time_period),
-    pattern = cross(abv_data, time_period)
+    data,
+    add_category(data_int2),
+    pattern = map(data_int2)
   ),
   tar_target(
-    filter3_cube,
-    filter_3(birdcube_data, time_period),
-    pattern = cross(birdcube_data, time_period)
+    filter1,
+    filter_1(data),
+    pattern = map(data)
+  ),
+  tar_target(
+    filter2,
+    filter_2(data, time_period),
+    pattern = cross(data, time_period)
+  ),
+  tar_target(
+    filter3,
+    filter_3(data, time_period),
+    pattern = cross(data, time_period)
   ),
   tar_target(
     range_comp_0,
-    range_comp_data(abv_data, birdcube_data),
-    pattern = map(abv_data, birdcube_data)
+    range_comp(data)
   ),
   tar_target(
     range_comp_1,
-    range_comp_data(filter1_abv, filter1_cube),
-    pattern = map(filter1_abv, filter1_cube)
+    range_comp(filter1)
   ),
   tar_target(
     range_comp_2,
-    range_comp_data(filter2_abv, filter2_cube),
-    pattern = map(filter2_abv, filter2_cube)
+    range_comp(filter2)
   ),
   tar_target(
     trend_comp_0,
