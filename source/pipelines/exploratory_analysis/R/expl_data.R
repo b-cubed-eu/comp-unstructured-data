c9b31eaea216c964546e866ec69a4497bc3cc350
EmmaCartuyvels1 <emma_cartuyvels@hotmail.com>
2025-04-11 14:22:12 +0200

    Rewrite functions to work in the branched target pipeline

--- source/pipelines/exploratory_analysis/R/expl_data.R
+++ source/pipelines/exploratory_analysis/R/expl_data.R
@@ -1,62 +1,33 @@
-range_comp <- function(dataset1,
-                       dataset2,
-                       sel_species,
-                       period = 2007:2022) {
-
-  # We filter both datasets for the species and period of interest
-  # and group them by TAG (identifier of utm square)
-  set_abv <- dataset1 |>
-    filter(.data$species %in% sel_species,
-           .data$year %in% period) |>
-    group_by(.data$mgrscode) |>
-    summarise(n = sum(.data$n))
-
-  set_cube <- dataset2 |>
-    filter(.data$species %in% sel_species,
-           .data$year %in% period) |>
-    group_by(.data$mgrscode) |>
-    summarise(n = sum(.data$n))
-
-  total_abv <- length(set_abv$mgrscode)
-  perc_abv <- (total_abv / length(unique(dataset1$mgrscode))) * 100
-
-  total_cube <- length(set_cube$mgrscode)
-  perc_cube <- (total_cube / length(unique(dataset2$mgrscode))) * 100
-
-  list(total_abv, perc_abv,
-       total_cube, perc_cube)
+my_group_by <- function(data, cols) {
+  group_by(data, pick({{ cols }}))
 }
 
-range_comp_data <- function(dataset1,
-                            dataset2,
-                            sel_species = unique(dataset1$species),
-                            period = 2007:2022){
+range_comp <- function(data) {
 
-  comp_range_data <- as.data.frame(sel_species)
-  comp_range_data$abv_squares <- NA
-  comp_range_data$perc_abv_total_abv <- NA
-  comp_range_data$cube_squares <- NA
-  comp_range_data$perc_cube_total_cube <- NA
-  if (assertthat::has_name(dataset1, "filter_per")){
-    comp_range_data$filter_per <- paste0(dataset1$filter_per[1],
-                                         dataset2$filter_per[1])
-  }
+  dataset_least_species <- data |>
+    group_by(id_dataset) |>
+    summarize(n_species = n_distinct(species)) |>
+    filter(n_species == min(n_species)) |>
+    pull(id_dataset)
 
-  comp_range_data <- comp_range_data |>
-    inner_join(dataset1 |> distinct(species, category),
-               by = join_by(sel_species == species))
+  species_list <- data |>
+    filter(id_dataset == dataset_least_species) |>
+    select(species) |>
+    distinct() |>
+    pull()
 
-  for (i in sel_species){
-    test <- range_comp(dataset1 = dataset1,
-                       dataset2 = dataset2,
-                       sel_species = i,
-                       period = period)
-
-    comp_range_data[comp_range_data$sel_species == i, 2] <- test[1]
-    comp_range_data[comp_range_data$sel_species == i, 3] <- test[2]
-    comp_range_data[comp_range_data$sel_species == i, 4] <- test[3]
-    comp_range_data[comp_range_data$sel_species == i, 5] <- test[4]
-  }
+  comp_range_data <- data |>
+    filter(species %in% species_list) |>
+    group_by(pick(matches("^id_"))) |>
+    mutate(tot_n_dist_gridcells = n_distinct(mgrscode)) |>
+    ungroup() |>
+    my_group_by(c(c(species, tot_n_dist_gridcells), matches("^id_"))) |>
+    summarise(n_dist_gridcells = n_distinct(mgrscode)) |>
+    ungroup() |>
+    mutate(percentage = n_dist_gridcells/tot_n_dist_gridcells) |>
+    pivot_wider(id_cols = c(id_spat_res, species, matches("^id_filter")),
+                names_from = id_dataset,
+                values_from = c(n_dist_gridcells, percentage))
 
   return(comp_range_data)
 }
@@ -82,7 +53,7 @@ trend_comp_data <- function(data1,
     group_by(species) |>
     summarize(correlation = cor(occurrence_1, occurrence_2,
                                 method = "pearson"))|>
-    inner_join(data1 |> distinct(species, category),
+    left_join(data1 |> distinct(species, category),
                by = join_by(species)) |>
     mutate(trend_comp_per = period)
 
