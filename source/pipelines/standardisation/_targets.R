166b0436b5b52e1de98de4c0392a4f3f0eec9d68
EmmaCartuyvels1 <emma_cartuyvels@hotmail.com>
2025-10-14 16:45:33 +0200

    Split up pipeline to allow more comparisons (i.e. with orginal abv data instead of filtered abv data)

--- source/pipelines/standardisation/_targets.R
+++ source/pipelines/standardisation/_targets.R
@@ -47,126 +47,181 @@ list(
   ),
   #' Define the data file target:
   tarchetypes::tar_file(
-    data_file,
+    data_file_abv,
     path_to_interim(path_to_data = path_to_data,
-                    dataset = dataset,
+                    dataset = "abv_data",
                     spat_res = spat_res),
-    pattern = cross(dataset, spat_res)
+    pattern = map(spat_res)
   ),
-  #' Read abv_1km, abv_10km, birdflanders_1km and birdflanders_10km:
+  tarchetypes::tar_file(
+    data_file_cube,
+    path_to_interim(path_to_data = path_to_data,
+                    dataset = "birdflanders",
+                    spat_res = spat_res),
+    pattern = map(spat_res)
+  ),
+  #' Read abv_1km and abv_10km:
   tar_target(
-    data_int1,
-    read_andid(data_file, dataset, spat_res),
-    pattern = map(data_file, cross(dataset, spat_res))
+    data_abv_int1,
+    read_andid(data_file_abv, "abv_data", spat_res),
+    pattern = map(data_file_abv, spat_res)
+  ),
+  #' Read birdflanders_1km and birdflanders_10km:
+  tar_target(
+    data_cube_int1,
+    read_andid(data_file_cube, "birdflanders", spat_res),
+    pattern = map(data_file_cube, spat_res)
   ),
   #' Add cyclus to datasets:
   tar_target(
-    data_int2,
-    add_cyclus(data_int1),
-    pattern = map(data_int1)
+    data_abv_int2,
+    add_cyclus(data_abv_int1),
+    pattern = map(data_abv_int1)
+  ),
+  tar_target(
+    data_cube,
+    add_cyclus(data_cube_int1),
+    pattern = map(data_cube_int1)
   ),
   #' Add category based on total number of observations per species:
   tar_target(
-    data,
-    add_category(data_int2),
-    pattern = map(data_int2)
+    data_abv,
+    add_category(data_abv_int2),
+    pattern = map(data_abv_int2)
   ),
   #' Filter for species interpreted under ABV scheme:
   tar_target(
-    filter1,
-    filter_1(data),
-    pattern = map(data)
+    filter1_abv,
+    filter_1(data_abv),
+    pattern = map(data_abv)
+  ),
+  tar_target(
+    filter1_cube,
+    filter_1(data_cube),
+    pattern = map(data_cube)
   ),
   #' Filter based on set of rules:
   tar_target(
-    filter2,
-    filter_2(data, time_period),
-    pattern = cross(data, time_period)
+    filter2_abv,
+    filter_2(data_abv, time_period),
+    pattern = cross(data_abv, time_period)
+  ),
+  tar_target(
+    filter2_cube,
+    filter_2(data_cube, time_period),
+    pattern = cross(data_cube, time_period)
   ),
   #' Convert to relative abundance per time period:
   tar_target(
-    filter3,
-    filter_3(data, time_period),
-    pattern = cross(data, time_period)
+    filter3_abv,
+    filter_3(data_abv, time_period),
+    pattern = cross(data_abv, time_period)
+  ),
+  tar_target(
+    filter3_cube,
+    filter_3(data_cube, time_period),
+    pattern = cross(data_cube, time_period)
   ),
   #' Apply filter 2 and convert to relative abundance per time period:
   tar_target(
-    filter4,
-    filter_3(filter2, time_period),
-    pattern = cross(filter2, time_period)
+    filter4_abv,
+    filter_3(filter2_abv, time_period),
+    pattern = cross(filter2_abv, time_period)
+  ),
+  tar_target(
+    filter4_cube,
+    filter_3(filter2_cube, time_period),
+    pattern = cross(filter2_cube, time_period)
   ),
   #' Convert to relative abundance per order:
   tar_target(
-    stand_order,
-    stand_class_level(data, stand_by = "ordercount", time_period),
-    pattern = cross(data, time_period)
+    stand_order_abv,
+    stand_class_level(data_abv, stand_by = "ordercount", time_period),
+    pattern = cross(data_abv, time_period)
+  ),
+  tar_target(
+    stand_order_cube,
+    stand_class_level(data_cube, stand_by = "ordercount", time_period),
+    pattern = cross(data_cube, time_period)
   ),
   #' Convert to relative abundance per family:
   tar_target(
-    stand_family,
-    stand_class_level(data, stand_by = "familycount", time_period),
-    pattern = cross(data, time_period)
+    stand_family_abv,
+    stand_class_level(data_abv, stand_by = "familycount", time_period),
+    pattern = cross(data_abv, time_period)
+  ),
+  tar_target(
+    stand_family_cube,
+    stand_class_level(data_cube, stand_by = "familycount", time_period),
+    pattern = cross(data_cube, time_period)
   ),
   #' Convert to relative abundance per genus:
   tar_target(
-    stand_genus,
-    stand_class_level(data, stand_by = "genuscount", time_period),
-    pattern = cross(data, time_period)
+    stand_genus_abv,
+    stand_class_level(data_abv, stand_by = "genuscount", time_period),
+    pattern = cross(data_abv, time_period)
   ),
+  tar_target(
+    stand_genus_cube,
+    stand_class_level(data_cube, stand_by = "genuscount", time_period),
+    pattern = cross(data_cube, time_period)
+  ),
+  #' Compare the range per species in structured and unstructured data
+  #' after applying different filters:
   #' Compare the range per species in structured and unstructured data
   #' after applying different filters:
   tar_target(
     range_comp_0,
-    range_comp(data)
+    range_comp(data_abv, data_cube)
   ),
   tar_target(
     range_comp_1,
-    range_comp(filter1)
+    range_comp(data_abv, filter1_cube)
   ),
   tar_target(
     range_comp_2,
-    range_comp(filter2)
+    range_comp(data_abv, filter2_cube)
   ),
   #' Compare the trend per species in structured and unstructured data
   #' after applying different filters:
   tar_target(
     trend_comp_0,
-    trend_comp(data, time_period),
+    trend_comp(data_abv, data_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_1,
-    trend_comp(filter1, time_period),
+    trend_comp(data_abv, filter1_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_2,
-    trend_comp(filter2, time_period),
+    trend_comp(data_abv, filter2_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_3,
-    trend_comp(filter3, time_period),
+    trend_comp(data_abv, filter3_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_4,
-    trend_comp(filter4, time_period),
+    trend_comp(data_abv, filter4_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_order,
-    trend_comp(stand_order, time_period),
+    trend_comp(data_abv, stand_order_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_family,
-    trend_comp(stand_family, time_period),
+    trend_comp(data_abv, stand_family_cube, time_period),
     pattern = map(time_period)
   ),
   tar_target(
     trend_comp_genus,
-    trend_comp(stand_genus, time_period),
+    trend_comp(data_abv, stand_genus_cube, time_period),
     pattern = map(time_period)
   )
 )
