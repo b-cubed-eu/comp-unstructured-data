6da6db0847c9a24732c8dd9c0ef1350988f989bb
Ward Langeraert <ward.langeraert@inbo.be>
2026-01-20 14:43:30 +0100

    iterate as list

--- source/pipelines/biodiversity_indicators/_targets.R
+++ source/pipelines/biodiversity_indicators/_targets.R
@@ -83,51 +83,60 @@ list(
   tar_target(
     obs_richness_map,
     obs_richness_map(data_cubes, cell_size = 10),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
   tar_target(
     obs_richness_ts,
     obs_richness_ts(data_cubes),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
   tar_target(
     total_occ_map,
     total_occ_map(data_cubes, cell_size = 10),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
   tar_target(
     total_occ_ts,
     total_occ_ts(data_cubes),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
-  tar_target(
-    pielou_evenness_map,
-    pielou_evenness_map(data_cubes, cell_size = 10),
-    pattern = map(data_cubes)
-  ),
-  tar_target(
-    pielou_evenness_ts,
-    pielou_evenness_ts(data_cubes),
-    pattern = map(data_cubes)
-  ),
+  # tar_target(
+  #   pielou_evenness_map,
+  #   pielou_evenness_map(data_cubes, cell_size = 10),
+  #   pattern = map(data_cubes),
+  #   error = "continue"
+  # ),
+  # tar_target(
+  #   pielou_evenness_ts,
+  #   pielou_evenness_ts(data_cubes),
+  #   pattern = map(data_cubes)
+  # ),
   tar_target(
     spec_occ_map,
     spec_occ_map(data_cubes),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
   tar_target(
     spec_occ_ts,
     spec_occ_ts(data_cubes),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
   tar_target(
     spec_range_map,
     spec_range_map(data_cubes),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   ),
   tar_target(
     spec_range_ts,
     spec_range_ts(data_cubes),
-    pattern = map(data_cubes)
+    pattern = map(data_cubes),
+    iteration = "list"
   )
 )
